969528f0fcaa5c06c5ace11f99ea048edcef3004
James Scouller <jscouller09@gmail.com>
2020-02-23 10:42:11 -0500

    Added icons and API response formatting

--- app/models/weather_station.rb
+++ app/models/weather_station.rb
@@ -1,3 +1,8 @@
+require 'open-uri'
+require 'json'
+require 'pry-byebug'
+require 'date'
+
 class WeatherStation < ActiveRecord::Base
   validates :name, presence: true
   validates :country, presence: true, format: { with: /[A-Z]{2}/ }
@@ -11,10 +16,73 @@ class WeatherStation < ActiveRecord::Base
   OW_BASE_URL = 'http://api.openweathermap.org/data'.freeze
   OW_API_KEY = 'e81081459c34b9d603d524e60453dedf'.freeze
 
+  WEATHER_CODES = {
+    200 => { main: 'Thunderstorm', description: 'thunderstorm with light rain', icon: '11d' },
+    201 => { main: 'Thunderstorm', description: 'thunderstorm with rain', icon: '11d' },
+    202 => { main: 'Thunderstorm', description: 'thunderstorm with heavy rain', icon: '11d' },
+    210 => { main: 'Thunderstorm', description: 'light thunderstorm', icon: '11d' },
+    211 => { main: 'Thunderstorm', description: 'thunderstorm', icon: '11d' },
+    212 => { main: 'Thunderstorm', description: 'heavy thunderstorm', icon: '11d' },
+    221 => { main: 'Thunderstorm', description: 'ragged thunderstorm', icon: '11d' },
+    230 => { main: 'Thunderstorm', description: 'thunderstorm with light drizzle', icon: '11d' },
+    231 => { main: 'Thunderstorm', description: 'thunderstorm with drizzle', icon: '11d' },
+    232 => { main: 'Thunderstorm', description: 'thunderstorm with heavy drizzle', icon: '11d' },
+
+    300 => { main: 'Drizzle', description: 'light intensity drizzle', icon: '09d' },
+    301 => { main: 'Drizzle', description: 'drizzle', icon: '09d' },
+    302 => { main: 'Drizzle', description: 'heavy intensity drizzle', icon: '09d' },
+    310 => { main: 'Drizzle', description: 'light intensity drizzle rain', icon: '09d' },
+    311 => { main: 'Drizzle', description: 'drizzle rain', icon: '09d' },
+    312 => { main: 'Drizzle', description: 'heavy intensity drizzle rain', icon: '09d' },
+    313 => { main: 'Drizzle', description: 'shower rain and drizzle', icon: '09d' },
+    314 => { main: 'Drizzle', description: 'heavy shower rain and drizzle', icon: '09d' },
+    321 => { main: 'Drizzle', description: 'shower drizzle', icon: '09d' },
+
+    500 => { main: 'Rain', description: 'light rain', icon: '10d' },
+    501 => { main: 'Rain', description: 'moderate rain', icon: '10d' },
+    502 => { main: 'Rain', description: 'heavy intensity rain', icon: '10d' },
+    503 => { main: 'Rain', description: 'very heavy rain', icon: '10d' },
+    504 => { main: 'Rain', description: 'extreme rain', icon: '10d' },
+    511 => { main: 'Rain', description: 'freezing rain', icon: '13d' },
+    520 => { main: 'Rain', description: 'light intensity shower rain', icon: '09d' },
+    521 => { main: 'Rain', description: 'shower rain', icon: '09d' },
+    522 => { main: 'Rain', description: 'heavy intensity shower rain', icon: '09d' },
+    531 => { main: 'Rain', description: 'ragged shower rain', icon: '09d' },
+
+    600 => { main: 'Snow', description: 'light snow', icon: '13d' },
+    601 => { main: 'Snow', description: 'snow', icon: '13d' },
+    602 => { main: 'Snow', description: 'heavy snow', icon: '13d' },
+    611 => { main: 'Snow', description: 'sleet', icon: '13d' },
+    612 => { main: 'Snow', description: 'light shower sleet', icon: '13d' },
+    613 => { main: 'Snow', description: 'shower sleet', icon: '13d' },
+    615 => { main: 'Snow', description: 'light rain and snow', icon: '13d' },
+    616 => { main: 'Snow', description: 'rain and snow', icon: '13d' },
+    620 => { main: 'Snow', description: 'light shower snow', icon: '13d' },
+    621 => { main: 'Snow', description: 'shower snow', icon: '13d' },
+    622 => { main: 'Snow', description: 'heavy shower snow', icon: '13d' },
+
+    701 => { main: 'Mist', description: 'mist', icon: '50d' },
+    711 => { main: 'Smoke', description: 'smoke', icon: '50d' },
+    721 => { main: 'Haze', description: 'haze', icon: '50d' },
+    731 => { main: 'Dust', description: 'sand/dust whirls', icon: '50d' },
+    741 => { main: 'Fog', description: 'fog', icon: '50d' },
+    751 => { main: 'Sand', description: 'sand', icon: '50d' },
+    761 => { main: 'Dust', description: 'dust', icon: '50d' },
+    762 => { main: 'Ash', description: 'volcanic ash', icon: '50d' },
+    771 => { main: 'Squall', description: 'squalls', icon: '50d' },
+    781 => { main: 'Tornado', description: 'tornado', icon: '50d' },
+
+    800 => { main: 'Clear', description: 'clear sky', icon: ['01d', '01n'] },
+    801 => { main: 'Clouds', description: 'few clouds (11-25%)', icon: ['02d', '02n'] },
+    802 => { main: 'Clouds', description: 'scattered clouds (25-50%)', icon: ['03d', '03n'] },
+    802 => { main: 'Clouds', description: 'broken clouds (50-85%)', icon: ['04d', '04n'] },
+    802 => { main: 'Clouds', description: 'overcast clouds (85-100%)', icon: ['04d', '04n'] },
+  }
+
   def download_current_data
     # build url
     url = "#{OW_BASE_URL}/2.5/weather?"
-    url += "id=#{args[:id]}&appid=#{OW_API_KEY}&units=metric"
+    url += "id=#{id}&appid=#{OW_API_KEY}&units=metric"
     # query API and return JSON
     serialised_data = URI.open(url).read
     data = JSON.parse(serialised_data, symbolize_names: true)
@@ -25,6 +93,26 @@ class WeatherStation < ActiveRecord::Base
   private
 
   def format_response(data = {})
-    data[:weather]
+    timestamp = DateTime.strptime(data[:dt].to_s,'%s')
+    data_to_keep = {}
+    data_to_keep[:dt_UTC] = data[:dt]
+    data_to_keep[:timestamp_UTC] = data[:sys][:dt_txt].nil? ? timestamp.strftime("%Y-%m-%d %H:%M:%S") : data[:sys][:dt_txt]
+    data_to_keep[:sunrise] = data[:sys][:sunrise]
+    data_to_keep[:sunset] = data[:sys][:sunset]
+    data_to_keep[:temp_c] = data[:main][:temp]
+    data_to_keep[:humidity_perc] = data[:main][:humidity]
+    data_to_keep[:pressure_hPa] = data[:main][:pressure]
+    data_to_keep[:wind_speed_mps] = data[:wind][:speed]
+    data_to_keep[:wind_direction_deg] = data[:wind][:deg]
+    data_to_keep[:cloudiness_perc] = data[:clouds][:all]
+    data_to_keep[:code] = data[:weather].first[:id]
+    data_to_keep[:main] = data[:weather].first[:main]
+    data_to_keep[:description] = data[:weather].first[:description]
+    data_to_keep[:icon] = data[:weather].first[:icon]
+    data_to_keep[:rain_1h_mm] = data[:rain].nil? ? nil : data[:rain][:"1h"]
+    data_to_keep[:rain_3h_mm] = data[:rain].nil? ? nil : data[:rain][:"3h"]
+    data_to_keep[:snow_1h_mm] = data[:snow].nil? ? nil : data[:snow][:"1h"]
+    data_to_keep[:snow_3h_mm] = data[:snow].nil? ? nil : data[:snow][:"3h"]
+    data_to_keep
   end
 end
