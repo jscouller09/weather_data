cc60dec4624faafd186bd999333441d7a717b5a5
James Scouller <jscouller09@gmail.com>
2020-02-22 22:03:01 -0500

    minor changes

--- app/models/weather_station.rb
+++ app/models/weather_station.rb
@@ -13,7 +13,8 @@ class WeatherStation < ActiveRecord::Base
 
   def download_current_data
     # build url
-    url = "#{OW_BASE_URL}/2.5/weather?id=#{args[:id]}&appid=#{OW_API_KEY}&units=metric"
+    url = "#{OW_BASE_URL}/2.5/weather?"
+    url += "id=#{args[:id]}&appid=#{OW_API_KEY}&units=metric"
     # query API and return JSON
     serialised_data = URI.open(url).read
     data = JSON.parse(serialised_data, symbolize_names: true)
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ CITY_INFO = JSON.parse(File.read('data/city.list.json'), symbolize_names: true)
 # iterate over station list and create WeatherStation instances
 invalid_stations = []
 counter = 0
+last_progress = 0
 num_stations = CITY_INFO.length
 CITY_INFO.each do |station|
   station.merge!(station[:coord])
@@ -15,8 +16,11 @@ CITY_INFO.each do |station|
   new_station = WeatherStation.new(station)
   invalid_stations << station unless new_station.save
   counter += 1
-  progress = counter.fdiv(num_stations) * 100.0
-  puts "#{progress.round(2)}\% complete"
+  progress = (counter.fdiv(num_stations) * 100.0).round
+  puts "#{progress}\% complete" unless progress == last_progress
+  last_progress = progress
 end
 
+# print invalid stations list at the end
+puts 'The following stations were invalid and not added to the DB...'
 invalid_stations.each { |stn| puts stn }
